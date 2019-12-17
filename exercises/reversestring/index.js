0d3aa61b04091b6d1417aaef46ee3bfc40bbae4d
Kevin Nam <kevinjnam@gmail.com>
2019-12-16 20:40:05 -0800

    completed reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,16 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+  reverse('apple') === 'leppa'
+  console.log(reverse('apple') === 'leppa');
+  reverse('hello') === 'olleh'
+  console.log(reverse('hello') === 'olleh');
+  reverse('Greetings!') === '!sgniteerG'
+  console.log(reverse('Greetings!') === '!sgniteerG');
 
-function reverse(str) {}
+  
+function reverse(str) {
+  return str.split('').reverse().join('')
+}
 
 module.exports = reverse;
