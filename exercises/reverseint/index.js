91f2ed9aa362a5226c0d4475b2222d3a5ac0fe04
Kevin Nam <kevinjnam@gmail.com>
2019-12-16 21:21:38 -0800

    refactored reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -2,23 +2,16 @@
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
 // --- Examples
-//   reverseInt(15) === 51
-//   reverseInt(981) === 189
-//   reverseInt(500) === 5
-//   reverseInt(-15) === -51
-//   reverseInt(-90) === -9
+  reverseInt(15) === 51
+  reverseInt(981) === 189
+  reverseInt(500) === 5
+  reverseInt(-15) === -51
+  reverseInt(-90) === -9
 
 function reverseInt(n) {
-  let numStr = n.toString();
-  if (n >= 0) {
-    numStr = numStr.split('').reverse().join('');
-    return Number(numStr);
-  } else {
-    numStr = numStr.slice(1);
-    numStr = numStr.split('').reverse().join('');
-    let num = Number(numStr);
-    return num - (2 * num);
-  }
+  const reversed = n.toString().split('').reverse().join('');
+  
+  return parseInt(reversed) * Math.sign(n)
 }
 
 module.exports = reverseInt;
