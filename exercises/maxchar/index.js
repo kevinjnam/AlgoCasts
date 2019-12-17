f0de20787d8ebed8632bd2874319f936b73aa620
Kevin Nam <kevinjnam@gmail.com>
2019-12-16 21:34:23 -0800

    completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const cache = {};
+  for (let char of str) {
+    cache[char] = cache[char] + 1 || 1;
+  }
+  let max = -Infinity;
+  let key;
+
+  for (let keys in cache) {
+    if (cache[keys] > max) {
+      max = cache[keys];
+      key = keys;
+    } 
+  }
+  return key;
+}
 
 module.exports = maxChar;
