64659ada738e1415bc5e12afedc7ab79c4491872
Kevin Nam <kevinjnam@gmail.com>
2019-12-17 19:13:57 -0800

    completed anagrams and chunk

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,23 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  if (stringA.length !== stringB.length) return false;
+
+  const cache = {};
+  let newA = stringA.replace(/[^\w]/g, '').toLowerCase();
+  let newB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+  for (let char of newA) {
+    cache[char] = cache[char] + 1 || 1;
+  }
+
+  for (let char of newB) {
+    if (!cache[char] || cache[char] === 0) return false;
+    cache[char] -= 1;
+  }
+  
+  return true;
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,30 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const results = [];
+  let index = 0;
+  while ( index < array.length ) {
+    results.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return results;
+}
+
+// function chunk(array, size) {
+//   const results = [];
+//   array.forEach(val => {
+//     const last = results[results.length - 1];
+//     if (!last || last.length === size) {
+//       results.push([val]);
+//     } else {
+//       last.push(val);
+//     }
+//   })
+//   return results;
+// }
+
+
 
 module.exports = chunk;
